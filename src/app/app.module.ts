e5c7d4f4c4418184f3545841e4c85f22bf21f079
ketevan kuchuloria <ketevankuchuloria@gmail.com>
2021-12-08 19:12:33 +0400

    translate

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+    },
+    defaultLanguage: 'en',
+  }),
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
